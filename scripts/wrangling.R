a1dd3ea11393f74dc5d09ead8dd844e3320b7c1b
Samantha Seibel <sls6550@psu.edu>
2023-02-08 10:10:47 -0500

    AMR_analytic_matrix output script update

--- scripts/wrangling.R
+++ scripts/wrangling.R
@@ -2,13 +2,16 @@
 #Samantha Seibel - last updated 20230823
 
 #load packages
-library()
-library()
-library()
+library(BiocManager)
+library(dplyr)
+library(stringr)
 
 
 #read in data
+out <- read.csv("amr-output/AMR_analytic_matrix.csv")
 
+#drop extension from file name
+str_split_i(colnames(out), "_", 1)
 
 
 #save work
