b27a300bfcc75ba6b4fe45fa7bcda332aedcbc6e
Samantha Seibel <sls6550@psu.edu>
2023-02-13 15:02:38 -0500

    analysis_workshop gene name corrections 2/13

--- scripts/wrangling.R
+++ scripts/wrangling.R
@@ -11,8 +11,42 @@ library(stringr)
 out <- read.csv("amr-output/AMR_analytic_matrix.csv")
 
 #drop extension from file name
-str_split_i(colnames(out), "_", 1)
+colnames(out) <- str_split_i(colnames(out), "_", 1)
 
+#break up gene information
+genes <- as.data.frame(rownames(out))
+genes <- as.data.frame(str_split_fixed(genes$`rownames(out)`, "\\|",5))
+colnames(genes) <- c("MEG_ID", "Broadclass", "Class", "Mechanism", "Gene")
 
+#merge gene info with counts
+out2 <- cbind(out, genes[5])
+out3 <- aggregate(.~Gene, data = out2, FUN = sum)
+out4 <- out3[2:ncol(out3)]
+rownames(out4) <-out3$Gene
+
+#write.table(out4, file = "tables/countmatrix-cleaned.txt", sep = "\t")
+
+#load packages for quick prelim plots
+library(ggplot2)
+library(data.table)
+
+meltdf <- melt(as.data.table(out3))
+
+ggplot(meltdf %>%
+         filter(value = !="0"), aes(x=variable, fill = variable, y = value)) +
+  geom_bar(stat =  "identity") +
+  scale_y_log10() +
+  facet_wrap(~Gene) +
+  theme(legend.position = "none")
+
+#heatmaps
+
+ggplot(meltdf %>%
+         filter(value = !="0"), aes(x=variable, fill = log(value), y = variable)) +
+  geom_tile() +
+  theme_bw() +
+  coord_fixed() + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  
 #save work
 save.image("data/wrangling.RData")
